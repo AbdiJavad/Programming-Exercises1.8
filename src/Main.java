2e165ae2241e795dd55bfa827194cd5bec4e21b2
AbdiJavad <Mirjavadabdi@gmail.com>
2023-12-01 15:08:53 +0330

    first commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Perimeter = ");
+        System.out.println(2 * 5.5 * 3.14);
+        System.out.println("Area = ");
+        System.out.println(5.5 * 5.5 * 3.14);
+    }
+}
